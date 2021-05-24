69aefd66481eb3d8185d555dee8235020cf0e9a1
taiwo_kenny <taiwokenny45@gmail.com>
2021-05-24 15:03:21 +0100

    repository and services completed

---
+++ lib/utils/menu.dart
@@ -0,0 +1,22 @@
+
+
+class Menu{
+  static const home ={
+    'app.menu.about': AboutScreen.route,
+    'app.menu.settings': SettingsScreen.route,
+  };
+
+  static const launch = [
+    'spacex.launch.menu.reddit',
+    'spacex.launch.menu.press_kit',
+  ];
+
+  static const wikipedia = [
+    'spacex.other.menu.wikipedia',
+  ];
+
+  static const ship = [
+    'spacex.other.menu.marine_traffic',
+  ];
+
+}
