823480e09b4e6f8ce25a838b0d420dddfb1e5733
taiwo_kenny <taiwokenny45@gmail.com>
2021-05-26 18:52:25 +0100

    setting up bloc observers

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:space_x/ui/views/general/index.dart';
 import 'package:path_provider/path_provider.dart';
-
 
 import 'cubits/index.dart';
 import 'repositories/index.dart';
@@ -13,16 +13,40 @@ import 'utils/index.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb
-        ? HydratedStorage.webStorageDirectory
-        : await getTemporaryDirectory()
-  );
-  Bloc.observer =
-  runApp(MyApp());
+      storageDirectory: kIsWeb
+          ? HydratedStorage.webStorageDirectory
+          : await getTemporaryDirectory());
+  Bloc.observer = CherryBlocObserver();
+
+  final httpClient = Dio();
+  runApp(MyApp(
+    vehiclesRepository: VehiclesRepository(VehiclesService(httpClient)),
+    launchRepository: LaunchRepository(LaunchesService(httpClient)),
+    achievementsRepository:
+        AchievementsRepository(AchievementsService(httpClient)),
+    changelogRepository: ChangelogRepository(ChangelogService(httpClient)),
+    companyRepository: CompanyRepository(CompanyService(httpClient)),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  final VehiclesRepository vehiclesRepository;
+  final LaunchRepository launchRepository;
+  final AchievementsRepository achievementsRepository;
+  final CompanyRepository companyRepository;
+  final ChangelogRepository changelogRepository;
+
   // This widget is the root of your application.
+
+  const MyApp({
+    Key key,
+    this.vehiclesRepository,
+    this.launchRepository,
+    this.achievementsRepository,
+    this.companyRepository,
+    this.changelogRepository,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,4 +58,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
