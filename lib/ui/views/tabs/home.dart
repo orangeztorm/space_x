0688b6aa932ed47a19be2c42aa5ccaa8c869cb64
taiwo_kenny <taiwokenny45@gmail.com>
2021-05-27 09:56:07 +0100

    swiper header

--- lib/ui/views/tabs/home.dart
+++ lib/ui/views/tabs/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-
+import '../../../cubits/index.dart';
 import '../../widgets/index.dart';
+import '../../../models/index.dart';
+import '../../../utils/index.dart';
 
 /// This tab holds main information about the next launch.
 /// It has a countdown widget.
@@ -16,18 +18,52 @@ class _HomeTabState extends State<HomeTab> {
   ScrollController _controller;
   double _offset = 0.0;
 
-
   @override
   void initState() {
     super.initState();
     _controller = ScrollController()
-    ..addListener(() => setState(() => _offset = _controller.offset));
+      ..addListener(() => setState(() => _offset = _controller.offset));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return RequestSliverPage<LaunchesCubit, List<List<Launch>>>(
+      controller: _controller,
+      title: context.translate('spacex.home.title'),
+      popupMenu: Menu.home,
+      headerBuilder: (context, state, value) => _HeaderView(
+          launch: LaunchUtils.getUpcomingLaunch(value), offset: _offset),
+      childrenBuilder: (context, state, value) => <Widget>[
+        SliverToBoxAdapter(
+          child: _HomeView(
+            LaunchUtils.getUpcomingLaunch(value),
+          ),
+        )
+      ],
+    );
+  }
+}
 
+class _HeaderView extends StatelessWidget {
+  final Launch launch;
+  final double offset;
+
+  const _HeaderView({Key key, this.launch, this.offset}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _sliverHeight =
+        MediaQuery.of(context).size.height * SliverBar.heightRatio;
+    final _isNotLandScape =
+        MediaQuery.of(context).orientation != Orientation.landscape;
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Opacity(
+            opacity: launch.isDateTooTentative && _isNotLandScape ? 1.0 : 0.64,
+        child: SwiperHeader(),
+        ),
+      ],
     );
   }
 }
--- lib/ui/widgets/index.dart
+++ lib/ui/widgets/index.dart
@@ -2,3 +2,4 @@ export 'custom_page.dart';
 export 'sliver_bar.dart';
 export 'loading_view.dart';
 export 'error_view.dart';
+export 'swiper_header.dart';
